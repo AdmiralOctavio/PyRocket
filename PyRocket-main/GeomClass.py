80bbc50ba3c973a65167597c979a8bccdd418f98
AOctavio <flaviocicero55@gmail.com>
2025-12-16 11:17:18 +0100

    Bug fix

--- PyRocket-main/GeomClass.py
+++ PyRocket-main/GeomClass.py
@@ -108,8 +108,9 @@ class ChamberGeometry():
         
         # This is meant to be able to import external CSV chamber contours.
         xy = np.genfromtxt('PyRocket-main\Contour.csv', delimiter = ',', skip_header=1) # Set directory as needed.
-        x = xy[:,0] / 1000
-        y = xy[:,1] / 1000
+        x = xy[:,0]
+        y = xy[:,1]
+        print(x)
         x_uniform = np.arange(x.min(), x.max(), self.dx)
         y_uniform = np.interp(x_uniform, x, y)
         self.x = x_uniform
